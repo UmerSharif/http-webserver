511c56ec27a330f552453d5cf2f023317f43dd7d
Umer Khan <umer4167@hotmail.com>
2023-03-08 15:17:36 +0200

    adding startup

---
+++ http/src/httprequest.rs
@@ -0,0 +1,62 @@
+use std::collections::HashMap;
+
+#[derive(Debug, PartialEq)]
+pub enum Resourse {
+    Path(String),
+}
+
+pub struct HttpRequest {
+    pub method: Method,
+    pub version: Version,
+    pub resourse: Resourse,
+    pub header: HashMap<String, String>,
+    pub msg_body: String,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum Method {
+    Get,
+    Post,
+    Uninitialized,
+}
+#[derive(Debug, PartialEq)]
+pub enum Version {
+    V1_1,
+    V2_2,
+    Uninitialized,
+}
+
+impl From<&str> for Method {
+    fn from(s: &str) -> Method {
+        match s {
+            "GET" => Method::Get,
+            "POST" => Method::Post,
+            _ => Method::Uninitialized,
+        }
+    }
+}
+
+impl From<&str> for Version {
+    fn from(s: &str) -> Version {
+        match s {
+            "HTTP/1.1" => Version::V1_1,
+            _ => Version::Uninitialized,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_method_into() {
+        let m: Method = "GET".into();
+        assert_eq!(m, Method::Get);
+    }
+
+    #[test]
+    fn test_version_into() {
+        let m: Version = "HTTP/1.1".into();
+        assert_eq!(m, Version::V1_1);
+    }
+}
